b858c8779c2759f96fc38ea5bd998cb9c46f806e
M4L5 <malin.gu2017@gmail.com>
2021-06-11 15:11:02 +0200

    added styled components in footer

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
+import styled from 'styled-components/macro'
+
+export const FooterContainer = styled.div`
+background-color: teal;
+height: 100px;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+export const Text = styled.h4`
+  color: white;
+  margin: 0 18px;
+`
+
+
 const Footer = () => {
     return(
-        <div className="footer">Footer</div>
+        <FooterContainer>
+          <Text>© Petspotter by Elaine Bergström and Malin Göthe</Text>
+        </FooterContainer>
     )
 }
 
