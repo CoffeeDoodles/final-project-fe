c06d83f67a835663e80985bf37775ea0d19a5eec
Elaine Bergstrom <Elainebergstrom26@gmail.com>
2021-06-23 00:54:16 +0200

    started styling details page

--- code/src/components/pages/PetPostDetails.js
+++ code/src/components/pages/PetPostDetails.js
@@ -1,24 +1,52 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
 
 const PetPostDetails = () => {
   const { id } = useParams();
-  const [details, setDetails] = useState([]);
+  const [details, setDetails] = useState({});
 
   const PET_DETAILS_URL = `http://localhost:8080/posts/${id}`
  
   useEffect(() => {
     fetch(PET_DETAILS_URL)
       .then((res) => res.json())
-      .then((json) => setDetails(json));
+      .then((data) => setDetails(data));
   }, [PET_DETAILS_URL]);
 
   return (
-    <div className="main-container">
-     WIP
-    </div>
+    <CardsWrapper className="main-container">
+     <p> Status: {details.status}</p>
+    </CardsWrapper>
   )
 }
 
 export default PetPostDetails
+
+const CardsWrapper = styled.main`
+  margin: 0;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+const Card = styled.article`
+  width: 300px;
+  height: 300px;
+  margin: 30px;
+  border-radius: 0 10px 10px 10px;
+  padding: 5px 10px 10px 5px;
+  background-color: #50b9cd;
+  box-sizing: border-box;
+  box-shadow: 0 8px 6px -6px grey;
+`;
+
+const InnerCard = styled.div`
+  border-radius: 0 10px 10px 10px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  background-color: white;
+`;
